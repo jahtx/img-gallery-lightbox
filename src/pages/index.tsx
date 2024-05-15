d416d9a3cc6ce239bc54000825f50bc23ecc8bc1
JHernandez <james@jahtx.com>
2024-05-15 13:24:23 -0500

    more changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import Title from '@/components/Title';
+import '@/styles/styles.scss';
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
+    <div className="homemade-container-sm mx-auto d-flex flex-column">
+      <Title>Hello LightBox</Title>
       <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
+        Mauris in augue felis. Phasellus aliquam sapien a arcu sollicitudin, vel
+        fringilla enim sollicitudin. Nullam augue ligula, ullamcorper in
+        ullamcorper condimentum, venenatis id dolor. Cras posuere nisl ac erat
+        ullamcorper viverra. Sed vulputate felis non tincidunt posuere. In et
+        purus libero. Vestibulum eu arcu vitae ligula fringilla maximus ac et
+        sem. Sed pulvinar dui et risus interdum pharetra. Etiam volutpat lorem
+        mauris, nec commodo neque tristique in. Duis ut interdum augue.
+        Curabitur elementum turpis vitae sapien blandit dapibus. Mauris congue
+        turpis in lacus condimentum, vel sagittis ex efficitur.
       </p>
-    </main>
+    </div>
   );
 }
